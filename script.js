8748d7a441b10216f83cb4833941cf5003c462fb
Alexandre Novaes <ale.oliveira00@gmail.com>
2024-11-11 01:17:31 -0300

    new

---
+++ script.js
@@ -0,0 +1,6 @@
+var menu = document.querySelector(".container-menu");
+var btn = document.querySelector("#btn-expandir");
+
+btn.addEventListener("click", function(){
+    menu.classList.toggle("expandir")
+});
