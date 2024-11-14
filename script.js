e51a0fb99ac9a0bf04a60d181c19e45014ef7a77
Alexandre Novaes <ale.oliveira00@gmail.com>
2024-11-14 01:47:57 -0300

    about

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ var btn = document.querySelector("#btn-expandir");
 btn.addEventListener("click", function(){
     menu.classList.toggle("expandir")
 });
+
+function toggleMenu() {
+    const menuLateral = document.getElementById('menuLateral');
+    const homePage = document.getElementById('homePage');
+    if (menuLateral && homePage) {
+        menuLateral.classList.toggle('expandir');
+        homePage.classList.toggle('expandir');
+    } else {
+        console.error('Elementos não encontrados');
+    }
+}
+
+document.getElementById('btn-bounce').addEventListener('click', function() {
+    this.classList.add('bounce');
+    setTimeout(() => {
+        this.classList.remove('bounce');
+    }, 500);
+});
