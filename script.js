81df4ec076161bc7c6147c8c1c4c0588dd643afd
Alexandre Novaes De Oliveira <ale.oliveira00@gmail.com>
2025-01-07 18:53:14 -0300

    ajuste

--- script.js
+++ script.js
@@ -7,22 +7,22 @@ btn.addEventListener("click", function(){
 });
 
 /////////////// function expandir conteudo tela //////////////////
-function toggleMenu() {
-    const menuLateral = document.getElementById('menuLateral');
-    const homePage = document.getElementById('homePage');
-    const aboutPage = document.getElementById('aboutPage');
-    const experiencePage = document.getElementById('experiencePage');
-    const developmentPage = document.getElementById('developmentPage');
-    if (menuLateral && homePage && aboutPage && experiencePage && developmentPage) {
-        menuLateral.classList.toggle('expandir');
-        homePage.classList.toggle('expandir');
-        aboutPage.classList.toggle('expandir');
-        experiencePage.classList.toggle('expandir');
-        developmentPage.classList.toggle('expandir');
-    } else {
-        console.error('Elementos não encontrados');
-    }
-}
+//function toggleMenu() {
+    //const menuLateral = document.getElementById('menuLateral');
+    //const homePage = document.getElementById('homePage');
+    //const aboutPage = document.getElementById('aboutPage');
+    //const experiencePage = document.getElementById('experiencePage');
+    //const developmentPage = document.getElementById('developmentPage');
+    //if (menuLateral && homePage && aboutPage && experiencePage && developmentPage) {
+        //menuLateral.classList.toggle('expandir');
+        //homePage.classList.toggle('expandir');
+        //aboutPage.classList.toggle('expandir');
+        //experiencePage.classList.toggle('expandir');
+        //developmentPage.classList.toggle('expandir');
+    //} else {
+        //console.error('Elementos não encontrados');
+    //}
+//}
 
 document.getElementById('btn-bounce').addEventListener('click', function() {
     this.classList.add('bounce');
