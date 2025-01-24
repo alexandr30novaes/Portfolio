8de3345445e651145480fa22741303c50b0b01c1
Alexandre Novaes De Oliveira <ale.oliveira00@gmail.com>
2025-01-24 15:15:00 -0300

    ajuste-experiencia-page

--- script.js
+++ script.js
@@ -71,3 +71,19 @@ function prevSlide() {
 
 // Inicializando o carrossel
 showSlide(currentIndex);
+
+
+// Animação escroll
+const elements = document.querySelectorAll('.hidden, .anim'); 
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+elements.forEach((el) => observer.observe(el))
